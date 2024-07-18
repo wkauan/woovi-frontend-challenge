ead0a9aa71583a660bc20406b74c00c2b9e6f4a3
wkauan <kauanmbomfim5@gmail.com>
2024-07-18 16:47:21 -0300

    feat: Nova página

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { PaymentMethod } from "./pages/payment-method";
+import { PixPaymentQrCode } from "./pages/pix-payment-qrcode";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <PaymentMethod />,
+  },
+  {
+    path: "/paymentQrCode",
+    element: <PixPaymentQrCode />,
   },
 ]);
 
