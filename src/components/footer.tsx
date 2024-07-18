5f4602b1e7826e7ab823a3e4e0c0ffe6df4c830d
wkauan <kauanmbomfim5@gmail.com>
2024-07-18 02:24:23 -0300

    feat: Adicionando components

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+import { ShieldCheck } from "lucide-react";
+
+export function Footer() {
+  return (
+    <footer className="flex gap-1.5 py-8 text-[#B2B2B2] text-xs">
+      <p className="flex gap-1.5">
+        <ShieldCheck className="size-4" />
+        Pagamento 100% seguro via:
+      </p>
+      <img className="h-4" src="./LogoGray.svg" alt="logo" />
+    </footer>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="flex items-center">
+      <img src="./Logo.svg" alt="Logo" />
+    </header>
+  );
+}
