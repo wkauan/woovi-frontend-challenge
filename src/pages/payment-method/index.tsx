9e6da92f0d49845fed60319f179e82fee74e2b7d
wkauan <kauanmbomfim5@gmail.com>
2024-07-18 02:24:09 -0300

    feat: Pagina inicial

---
+++ src/pages/payment-method/index.tsx
@@ -0,0 +1,44 @@
+import { ArrowRight } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { Header } from "../../components/header";
+import { PixInput } from "./pix-input";
+import { PixInstallments } from "./pix-installments";
+import { Footer } from "../../components/footer";
+
+export function PaymentMethod() {
+  const navigate = useNavigate();
+
+  function nextPage() {
+    navigate("/next");
+  }
+
+  return (
+    <div className="max-w-sm bg-white">
+      <div className="flex items-center justify-center py-8 flex-col">
+        <Header />
+
+        <div className="py-8">
+          <h2 className="text-lg font-semibold ">
+            João, como você quer pagar?
+          </h2>
+        </div>
+
+        <PixInput />
+
+        <PixInstallments />
+
+        <div>
+          <button
+            onClick={nextPage}
+            className="bg-[#03D69D] text-white rounded-xl px-2 py-0.5 font-semibold flex gap-2"
+          >
+            Continuar
+            <ArrowRight />
+          </button>
+        </div>
+
+        <Footer />
+      </div>
+    </div>
+  );
+}
