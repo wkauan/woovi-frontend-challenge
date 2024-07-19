0197d539410867b326ce9479a8efe35c8853776c
wkauan <kauanmbomfim5@gmail.com>
2024-07-18 22:05:21 -0300

    refactor: Seperando em outro arquivo os dados

---
+++ src/lib/data.ts
@@ -0,0 +1,44 @@
+export const paymentOptions = [
+  {
+    installments: 1,
+    amount: "R$ 30.500,00",
+    total: "R$ 30.500,00",
+    bestOption: false,
+  },
+  {
+    installments: 2,
+    amount: "R$ 15.300,00",
+    total: "R$ 30.600,00",
+    bestOption: false,
+  },
+  {
+    installments: 3,
+    amount: "R$ 10.196,66",
+    total: "R$ 30.620,00",
+    bestOption: false,
+  },
+  {
+    installments: 4,
+    amount: "R$ 7.725,00",
+    total: "R$ 30.900,00",
+    bestOption: true,
+  },
+  {
+    installments: 5,
+    amount: "R$ 6.300,00",
+    total: "R$ 31.500,00",
+    bestOption: false,
+  },
+  {
+    installments: 6,
+    amount: "R$ 5.283,33",
+    total: "R$ 31.699,98",
+    bestOption: false,
+  },
+  {
+    installments: 7,
+    amount: "R$ 4.542,85",
+    total: "R$ 31.800,00",
+    bestOption: false,
+  },
+];
--- src/pages/payment-method/pix-input.tsx
+++ src/pages/payment-method/pix-input.tsx
@@ -1,50 +1,7 @@
 import { Circle, CircleCheck } from "lucide-react";
 import { useState } from "react";
 
-const paymentOptions = [
-  {
-    installments: 1,
-    amount: "R$ 30.500,00",
-    total: "R$ 30.500,00",
-    bestOption: false,
-  },
-  {
-    installments: 2,
-    amount: "R$ 15.300,00",
-    total: "R$ 30.600,00",
-    bestOption: false,
-  },
-  {
-    installments: 3,
-    amount: "R$ 10.196,66",
-    total: "R$ 30.620,00",
-    bestOption: false,
-  },
-  {
-    installments: 4,
-    amount: "R$ 7.725,00",
-    total: "R$ 30.900,00",
-    bestOption: true,
-  },
-  {
-    installments: 5,
-    amount: "R$ 6.300,00",
-    total: "R$ 31.500,00",
-    bestOption: false,
-  },
-  {
-    installments: 6,
-    amount: "R$ 5.283,33",
-    total: "R$ 31.699,98",
-    bestOption: false,
-  },
-  {
-    installments: 7,
-    amount: "R$ 4.542,85",
-    total: "R$ 31.800,00",
-    bestOption: false,
-  },
-];
+import { paymentOptions } from "../../lib/data";
 
 export function PixInput() {
   const [selectedOption, setSelectedOption] = useState<number>(1);
