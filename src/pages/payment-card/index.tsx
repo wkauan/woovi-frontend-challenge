48bf9a1337fac7fea568f27a98a9a9ab47e83dc6
wkauan <kauanmbomfim5@gmail.com>
2024-07-19 18:11:43 -0300

    fix: Corrigindo width

--- src/pages/payment-card/index.tsx
+++ src/pages/payment-card/index.tsx
@@ -15,39 +15,41 @@ export function PaymentCard() {
     navigate("/");
   }
   return (
-    <div className="w-full flex items-center justify-center py-8 flex-col">
-      <Header />
+    <div className="">
+      <div className="flex items-center justify-center py-8 flex-col">
+        <Header />
 
-      <div className="py-8">
-        <h2 className="text-2xl font-extrabold">
-          João, pague o restante em 1x no cartão
-        </h2>
+        <div className="py-8">
+          <h2 className="text-2xl font-extrabold">
+            João, pague o restante em 1x no cartão
+          </h2>
+        </div>
+
+        <div className="w-full">
+          <FormCheckout />
+        </div>
+
+        <div className="py-8 w-full">
+          <Button size="full" variant="secondary">
+            Pagar
+          </Button>
+        </div>
+
+        <PaymentDeadline />
+
+        <PaymentDetails isChecked={true} total={"31.500,00"} />
+
+        <PaymentId />
+
+        <div className="py-8">
+          <Button onClick={nextPage}>
+            Continuar
+            <ArrowRight />
+          </Button>
+        </div>
+
+        <Footer />
       </div>
-
-      <div className="w-full">
-        <FormCheckout />
-      </div>
-
-      <div className="py-8 w-full">
-        <Button size="full" variant="secondary">
-          Pagar
-        </Button>
-      </div>
-
-      <PaymentDeadline />
-
-      <PaymentDetails isChecked={true} total={"31.500,00"} />
-
-      <PaymentId />
-
-      <div className="py-8">
-        <Button onClick={nextPage}>
-          Continuar
-          <ArrowRight />
-        </Button>
-      </div>
-
-      <Footer />
     </div>
   );
 }
